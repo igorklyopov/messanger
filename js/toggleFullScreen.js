f42a949165241c3a8d5a3f7f14bef2363bb1ccd4
Igor Klyopov <shoot8202@gmail.com>
2022-05-13 14:43:03 +0300

    improve toggleFullScreen

--- js/toggleFullScreen.js
+++ js/toggleFullScreen.js
@@ -2,24 +2,43 @@ const pageWrapRef = document.getElementById('page-wrap');
 const headerRef = document.getElementById('header');
 const mainRef = document.getElementById('main');
 
-function toggleFullScreen() {
-  if (
-    pageWrapRef.clientWidth === document.body.clientWidth &&
-    pageWrapRef.clientHeight === document.body.clientHeight
-  ) {
-    pageWrapRef.style.width = null;
-    pageWrapRef.style.height = null;
-    mainRef.style.height = null;
-    mainRef.style.maxHeight = null;
+const isFullScreen = () =>
+  pageWrapRef.clientWidth === document.body.clientWidth &&
+  pageWrapRef.clientHeight === document.body.clientHeight;
 
-    return;
-  }
+const fullscreen = localStorage.getItem('fullscreen');
+
+function makeFullScreen() {
   pageWrapRef.style.width = document.body.clientWidth + 'px';
   pageWrapRef.style.height = document.body.clientHeight + 'px';
   mainRef.style.height =
     document.body.clientHeight - headerRef.offsetHeight + 'px';
   mainRef.style.maxHeight =
     document.body.clientHeight - headerRef.offsetHeight + 'px';
+
+  localStorage.setItem('fullscreen', 'yes');
+}
+
+function makeSmallScreen() {
+  pageWrapRef.style.width = null;
+  pageWrapRef.style.height = null;
+  mainRef.style.height = null;
+  mainRef.style.maxHeight = null;
+
+  localStorage.setItem('fullscreen', 'no');
+}
+
+function toggleFullScreen() {
+  if (isFullScreen()) {
+    makeSmallScreen();
+    return;
+  } else {
+    makeFullScreen();
+  }
+}
+
+if (fullscreen === 'yes') {
+  makeFullScreen();
 }
 
 const changePageSizeBtnRef = document.querySelector('.js-change-page-size-btn');
